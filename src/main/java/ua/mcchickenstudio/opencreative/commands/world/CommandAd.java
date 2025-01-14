8123ef74f0760831bd4b8e5b4ff4a0aed389c894
onn512 <onn.onn512@gmail.com>
2025-01-14 06:12:20 +0100

    Refactor CommandAd

--- src/main/java/ua/mcchickenstudio/opencreative/commands/world/CommandAd.java
+++ src/main/java/ua/mcchickenstudio/opencreative/commands/world/CommandAd.java
@@ -18,13 +18,12 @@
 
 package ua.mcchickenstudio.opencreative.commands.world;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
 import ua.mcchickenstudio.opencreative.OpenCreative;
 import ua.mcchickenstudio.opencreative.coding.blocks.events.EventRaiser;
 import ua.mcchickenstudio.opencreative.events.planet.PlanetAdvertisementEvent;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
@@ -38,134 +37,194 @@ import ua.mcchickenstudio.opencreative.planets.Planet;
 import ua.mcchickenstudio.opencreative.planets.PlanetManager;
 import ua.mcchickenstudio.opencreative.utils.CooldownUtils;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static ua.mcchickenstudio.opencreative.utils.CooldownUtils.getCooldown;
 import static ua.mcchickenstudio.opencreative.utils.CooldownUtils.setCooldown;
-import static ua.mcchickenstudio.opencreative.utils.MessageUtils.getLocaleMessage;
-import static ua.mcchickenstudio.opencreative.utils.MessageUtils.parsePlanetLines;
+import static ua.mcchickenstudio.opencreative.utils.MessageUtils.*;
 
 public class CommandAd implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-        if (sender instanceof Player player) {
-            Planet planet = PlanetManager.getInstance().getPlanetByPlayer(player);
-            if (OpenCreative.getSettings().isMaintenance() && !player.hasPermission("opencreative.maintenance.bypass")) {
-                player.sendMessage(getLocaleMessage("maintenance"));
-                return true;
-            }
-            if (args.length >= 1) {
-                if (getCooldown(player, CooldownUtils.CooldownType.GENERIC_COMMAND) > 0) {
-                    player.sendMessage(getLocaleMessage("cooldown").replace("%cooldown%",String.valueOf(getCooldown(player,CooldownUtils.CooldownType.GENERIC_COMMAND))));
-                    return true;
-                }
-                setCooldown(player, OpenCreative.getSettings().getGroups().getGroup(player).getGenericCommandCooldown(), CooldownUtils.CooldownType.GENERIC_COMMAND);
-                if (!PlanetManager.getInstance().getPlanets().isEmpty()) {
-                    Planet foundPlanet = null;
-                    for (Planet searchablePlanet : PlanetManager.getInstance().getPlanets()) {
-                        if (String.valueOf(searchablePlanet.getId()).equals(args[0])) {
-                            foundPlanet = searchablePlanet;
-                            break;
-                        } else if (searchablePlanet.getInformation().getCustomID().equalsIgnoreCase(args[0])) {
-                            foundPlanet = searchablePlanet;
-                            break;
-                        }
-                    }
-                    if (foundPlanet != null) {
-                        if (args.length == 2) {
-                            Player inviteReceiver = Bukkit.getPlayer(args[1]);
-                            if (inviteReceiver == null) {
-                                player.sendMessage(getLocaleMessage("no-player-found",player));
-                                return true;
-                            }
-                            if (player.equals(inviteReceiver)) {
-                                player.sendMessage(getLocaleMessage("same-world",player));
-                                return true;
-                            }
-                            if (foundPlanet.getSharing() != Planet.Sharing.PUBLIC) {
-                                player.sendMessage(getLocaleMessage("advertisement.closed-world"));
-                                return true;
-                            }
-                            if (getCooldown(player, CooldownUtils.CooldownType.ADVERTISEMENT_COMMAND) > 0) {
-                                player.sendMessage(getLocaleMessage("advertisement.cooldown").replace("%cooldown%",String.valueOf(getCooldown(player,CooldownUtils.CooldownType.ADVERTISEMENT_COMMAND))));
-                                return true;
-                            }
-                            PlanetInviteEvent event = new PlanetInviteEvent(foundPlanet,player,inviteReceiver);
-                            event.callEvent();
-                            if (event.isCancelled()) return true;
-                            setCooldown(player, OpenCreative.getSettings().getGroups().getGroup(player).getAdvertisementCooldown(), CooldownUtils.CooldownType.ADVERTISEMENT_COMMAND);
-                            TextComponent advertisement = new TextComponent(getLocaleMessage("advertisement.message",player).replace("%world%", foundPlanet.getInformation().getDisplayName()));
-                            advertisement.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(parsePlanetLines(foundPlanet,getLocaleMessage("advertisement.hover")))));
-                            advertisement.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/ad " + foundPlanet.getId()));
-                            inviteReceiver.sendMessage(advertisement);
-                            player.sendMessage(advertisement);
-                        } else if (foundPlanet.equals(PlanetManager.getInstance().getPlanetByPlayer(player))) {
-                            player.sendMessage(getLocaleMessage("same-world",player));
-                        } else {
-                            foundPlanet.connectPlayer(player);
-                        }
-                    } else {
-                        player.playSound(player.getLocation(),Sound.BLOCK_ANVIL_DESTROY,100,2);
-                        player.clearTitle();
-                        player.sendMessage(getLocaleMessage("no-planet-found",player));
-                    }
-                } else {
-                    player.playSound(player.getLocation(),Sound.BLOCK_ANVIL_DESTROY,100,2);
-                    player.clearTitle();
-                    player.sendMessage(getLocaleMessage("no-planet-found",player));
-                }
-            } else {
-                if (planet == null) {
-                    player.sendMessage(getLocaleMessage("only-in-world"));
-                    return true;
-                }
-                if (getCooldown(player, CooldownUtils.CooldownType.ADVERTISEMENT_COMMAND) > 0) {
-                    player.sendMessage(getLocaleMessage("advertisement.cooldown").replace("%cooldown%",String.valueOf(getCooldown(player,CooldownUtils.CooldownType.ADVERTISEMENT_COMMAND))));
-                    return true;
-                }
-                if (!(planet.getSharing() == Planet.Sharing.PUBLIC)) {
-                    player.sendMessage(getLocaleMessage("advertisement.closed-world"));
-                    return true;
-                }
-                PlanetAdvertisementEvent event = new PlanetAdvertisementEvent(planet,player);
-                event.callEvent();
-                if (event.isCancelled()) return true;
-                setCooldown(player, OpenCreative.getSettings().getGroups().getGroup(player).getAdvertisementCooldown(), CooldownUtils.CooldownType.ADVERTISEMENT_COMMAND);
-                if (OpenCreative.getEconomy().isEnabled()) {
-                    double playerBalance = OpenCreative.getEconomy().getBalance(player).doubleValue();
-                    double advertisementPrice = OpenCreative.getSettings().getGroups().getGroup(player).getAdvertisementPrice();
-                    if (playerBalance < advertisementPrice) {
-                        player.sendMessage(getLocaleMessage("advertisement.no-money",player).replace("%money%",String.valueOf(Math.round(advertisementPrice-playerBalance))));
-                        return true;
-                    } else {
-                        OpenCreative.getEconomy().withdrawMoney(player,advertisementPrice);
-                    }
-                }
-                EventRaiser.raiseAdvertisedEvent(player);
-                for (Player p : Bukkit.getOnlinePlayers()) {
-                    TextComponent advertisement = new TextComponent(getLocaleMessage("advertisement.message",player).replace("%world%", planet.getInformation().getDisplayName()));
-                    advertisement.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(parsePlanetLines(planet,getLocaleMessage("advertisement.hover")))));
-                    advertisement.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/ad " + planet.getId()));
-                        p.sendMessage(advertisement);
-                }
-            }
+        if (!(sender instanceof Player player)) {
+            return true;
+        }
+
+        if (OpenCreative.getSettings().isMaintenance() && !player.hasPermission("opencreative.maintenance.bypass")) {
+            player.sendMessage(getLocaleMessage("maintenance"));
+            return true;
+        }
+
+        switch (args.length) {
+            case 0:  // /ad
+                handlePlanetAdvertisement(player);
+                break;
+            case 1:  // /ad [planet id]
+                handlePlayerConnection(player, args[0]);
+                break;
+            case 2:  // /ad [planet id] [player]
+                handlePlanetInvitation(player, args[0], args[1]);
+                break;
         }
 
         return true;
     }
 
-    @Override
-    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
-        if (args.length == 1) {
-            List<String> TabCompleter = new ArrayList<>();
-            for (Planet planet : PlanetManager.getInstance().getPlanets()) {
-                TabCompleter.add(planet.getInformation().getCustomID());
+    private void handlePlanetAdvertisement(Player player) {
+        Planet planet = PlanetManager.getInstance().getPlanetByPlayer(player);
+
+        if (planet == null) {
+            player.sendMessage(getLocaleMessage("only-in-world"));
+            return;
+        }
+
+        if (getCooldown(player, CooldownUtils.CooldownType.ADVERTISEMENT_COMMAND) > 0) {
+            player.sendMessage(getLocaleMessage("advertisement.cooldown").replace("%cooldown%", String.valueOf(getCooldown(player, CooldownUtils.CooldownType.ADVERTISEMENT_COMMAND))));
+            return;
+        }
+
+        if (!(planet.getSharing() == Planet.Sharing.PUBLIC)) {
+            player.sendMessage(getLocaleMessage("advertisement.closed-world"));
+            return;
+        }
+
+        PlanetAdvertisementEvent event = new PlanetAdvertisementEvent(planet, player);
+        event.callEvent();
+
+        if (event.isCancelled()) return;
+
+        if (OpenCreative.getEconomy().isEnabled()) {
+            double playerBalance = OpenCreative.getEconomy().getBalance(player).doubleValue();
+            double advertisementPrice = OpenCreative.getSettings().getGroups().getGroup(player).getAdvertisementPrice();
+
+            if (playerBalance < advertisementPrice) {
+                player.sendMessage(getLocaleMessage("advertisement.no-money", player)
+                        .replace("%money%", String.valueOf(Math.round(advertisementPrice - playerBalance))));
+                return;
+            } else {
+                OpenCreative.getEconomy().withdrawMoney(player, advertisementPrice);
             }
-            return TabCompleter;
+        }
+
+        setCooldown(player, OpenCreative.getSettings().getGroups().getGroup(player).getAdvertisementCooldown(), CooldownUtils.CooldownType.ADVERTISEMENT_COMMAND);
+        EventRaiser.raiseAdvertisedEvent(player);
+
+        Component advertisementMessage = createAdvertisementMessage(player, planet);
+
+        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+            onlinePlayer.sendMessage(advertisementMessage);
+        }
+    }
+
+    private void handlePlayerConnection(Player player, String planetId) {
+        if (getCooldown(player, CooldownUtils.CooldownType.GENERIC_COMMAND) > 0) {
+            player.sendMessage(getLocaleMessage("cooldown").replace("%cooldown%", String.valueOf(getCooldown(player, CooldownUtils.CooldownType.GENERIC_COMMAND))));
+            return;
+        }
+        setCooldown(player, OpenCreative.getSettings().getGroups().getGroup(player).getGenericCommandCooldown(), CooldownUtils.CooldownType.GENERIC_COMMAND);
+
+        Planet foundPlanet = findPlanet(planetId);
+
+        if (foundPlanet == null) {
+            player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_DESTROY, 100, 2);
+            player.clearTitle();
+            player.sendMessage(getLocaleMessage("no-planet-found", player));
+            return;
+        }
+
+        if (foundPlanet.equals(PlanetManager.getInstance().getPlanetByPlayer(player))) {
+            player.sendMessage(getLocaleMessage("same-world", player));
+            return;
+        }
+
+        foundPlanet.connectPlayer(player);
+    }
+
+    private void handlePlanetInvitation(Player player, String planetId, String inviteeName) {
+        Player inviteReceiver = Bukkit.getPlayer(inviteeName);
+
+        if (inviteReceiver == null) {
+            player.sendMessage(getLocaleMessage("no-player-found", player));
+            return;
+        }
+
+        if (player.equals(inviteReceiver)) {
+            player.sendMessage(getLocaleMessage("same-world", player));
+            return;
+        }
+
+        Planet foundPlanet = findPlanet(planetId);
+
+        if (foundPlanet == null) {
+            player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_DESTROY, 100, 2);
+            player.clearTitle();
+            player.sendMessage(getLocaleMessage("no-planet-found", player));
+            return;
+        }
+
+        if (foundPlanet.getSharing() != Planet.Sharing.PUBLIC) {
+            player.sendMessage(getLocaleMessage("advertisement.closed-world"));
+            return;
+        }
+
+        if (getCooldown(player, CooldownUtils.CooldownType.ADVERTISEMENT_COMMAND) > 0) {
+            player.sendMessage(getLocaleMessage("advertisement.cooldown").replace("%cooldown%", String.valueOf(getCooldown(player, CooldownUtils.CooldownType.ADVERTISEMENT_COMMAND))));
+            return;
+        }
+
+        PlanetInviteEvent event = new PlanetInviteEvent(foundPlanet, player, inviteReceiver);
+        event.callEvent();
+
+        if (event.isCancelled()) return;
+
+        setCooldown(player, OpenCreative.getSettings().getGroups().getGroup(player).getAdvertisementCooldown(), CooldownUtils.CooldownType.ADVERTISEMENT_COMMAND);
+
+        Component advertisementMessage = createAdvertisementMessage(player, foundPlanet);
+        inviteReceiver.sendMessage(advertisementMessage);
+        player.sendMessage(advertisementMessage);
+    }
+
+    private Component createAdvertisementMessage(Player player, Planet planet) {
+        String advertisementText = getLocaleMessage("advertisement.message", player)
+                .replace("%world%", planet.getInformation().getDisplayName());
+        String hoverText = parsePlanetLines(planet, getLocaleMessage("advertisement.hover"));
+        String clickCommand = "/ad " + planet.getId();
+
+        Component advertisement = toComponent(advertisementText);
+        Component hoverComponent = toComponent(hoverText);
+
+        advertisement = advertisement
+                .hoverEvent(HoverEvent.showText(hoverComponent))
+                .clickEvent(ClickEvent.runCommand(clickCommand));
+
+        return advertisement;
+    }
+
+    private Planet findPlanet(String planetId) {
+        if (PlanetManager.getInstance().getPlanets().isEmpty()) return null;
+
+        for (Planet searchablePlanet : PlanetManager.getInstance().getPlanets()) {
+            if (String.valueOf(searchablePlanet.getId()).equals(planetId) ||
+                    searchablePlanet.getInformation().getCustomID().equalsIgnoreCase(planetId)) {
+                return searchablePlanet;
+            }
         }
         return null;
     }
 
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
+        return switch (args.length) {
+            case 1 -> PlanetManager.getInstance().getPlanets().stream()
+                    .map(planet -> planet.getInformation().getCustomID())
+                    .toList();
+            case 2 -> Bukkit.getOnlinePlayers().stream()
+                    .map(Player::getName)
+                    .toList();
+            default -> Collections.emptyList();
+        };
+    }
 }
+
