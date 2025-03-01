5fd97ad36d61323bd2a331624ee1d85a52a641c2
nosatlev <mcchickenstudio@gmail.com>
2025-03-01 21:31:35 +0200

    Added 3 entity and 3 player actions: Release Shoulders, Set Remaining Air, Set Maximum Air

---
+++ src/main/java/ua/mcchickenstudio/opencreative/coding/blocks/actions/playeractions/params/PlayerSetMaximumAirAction.java
@@ -0,0 +1,51 @@
+/*
+ * OpenCreative+, Minecraft plugin.
+ * (C) 2022-2025, McChicken Studio, mcchickenstudio@gmail.com
+ *
+ * OpenCreative+ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenCreative+ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ua.mcchickenstudio.opencreative.coding.blocks.actions.playeractions.params;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import ua.mcchickenstudio.opencreative.coding.arguments.Arguments;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.ActionType;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.Target;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.entityactions.EntityAction;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.playeractions.PlayerAction;
+import ua.mcchickenstudio.opencreative.coding.blocks.executors.Executor;
+
+public final class PlayerSetMaximumAirAction extends PlayerAction {
+    public PlayerSetMaximumAirAction(Executor executor, Target target, int x, Arguments args) {
+        super(executor, target, x, args);
+    }
+
+    @Override
+    public void executePlayer(Player player) {
+        boolean add = getArguments().getValue("add",false,this);
+        int ticks = getArguments().getValue("ticks",0,this);
+        if (!add) {
+            player.setMaximumAir(ticks);
+        } else {
+            player.setMaximumAir(player.getMaximumAir()+ticks);
+        }
+    }
+
+    @Override
+    public ActionType getActionType() {
+        return ActionType.PLAYER_SET_MAXIMUM_AIR;
+    }
+}
