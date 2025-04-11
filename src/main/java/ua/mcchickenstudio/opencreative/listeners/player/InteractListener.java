abdcf36f936e4287ee697b65d726bb75ada6360f
nosatlev <mcchickenstudio@gmail.com>
2025-04-11 21:02:27 +0300

    Debug: Fix special blocks menus

--- src/main/java/ua/mcchickenstudio/opencreative/listeners/player/InteractListener.java
+++ src/main/java/ua/mcchickenstudio/opencreative/listeners/player/InteractListener.java
@@ -272,12 +272,26 @@ public final class InteractListener implements Listener {
             // FIXME: Debug!!!
             if (OpenCreative.getSettings().isDebug()) {
                 if (mainBlockCategory != null) {
-                    menu = new MenusCategorySelectionMenu(player, clickedBlock.getLocation(), mainBlockCategory);
+                    menu = switch (mainBlockCategory) {
+                        case EVENT_WORLD, EVENT_ENTITY, EVENT_PLAYER -> new MenusCategorySelectionMenu(player, clickedBlock.getLocation(), mainBlockCategory);
+                        default -> null;
+                    };
                 } else if (actionBlockCategory != null) {
-                    menu = new MenusCategorySelectionMenu(player, clickedBlock.getLocation(), actionBlockCategory);
+                    menu = switch (actionBlockCategory) {
+                        case PLAYER_ACTION, PLAYER_CONDITION, VARIABLE_ACTION,
+                             VARIABLE_CONDITION, ENTITY_ACTION, ENTITY_CONDITION,
+                             WORLD_ACTION, WORLD_CONDITION, CONTROL_ACTION,
+                             REPEAT_ACTION, HANDLER_ACTION -> new MenusCategorySelectionMenu(player, clickedBlock.getLocation(), actionBlockCategory);
+                        case SELECTION_ACTION -> new SelectionActionsMenu(player,clickedBlock.getLocation());
+                        case LAUNCH_FUNCTION_ACTION -> new FunctionChooserMenu(player, devPlanet,clickedBlock.getLocation());
+                        case LAUNCH_METHOD_ACTION -> new MethodChooserMenu(player, devPlanet,clickedBlock.getLocation());
+                        default -> null;
+                    };
                 }
-                if (menu != null) menu.open(player);
-                return;
+                if (menu != null) {
+                    menu.open(player);
+                    return;
+                }
             }
             // FIXME: Debug!!!
             if (mainBlockCategory != null) {
--- src/main/java/ua/mcchickenstudio/opencreative/utils/hooks/PAPIUtils.java
+++ src/main/java/ua/mcchickenstudio/opencreative/utils/hooks/PAPIUtils.java
@@ -112,7 +112,7 @@ class Placeholder extends PlaceholderExpansion {
 
     @Override
     public @NotNull String getVersion() {
-        return "5.5";
+        return "5.6";
     }
 
     @Override
