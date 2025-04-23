99dbf1e1de9f94465d3e98d845ce87e9e239cd45
nosatlev <mcchickenstudio@gmail.com>
2025-04-23 15:33:35 +0300

    Add EntityNameEqualsCondition

---
+++ src/main/java/ua/mcchickenstudio/opencreative/coding/blocks/conditions/entityconditions/other/EntityNameEqualsCondition.java
@@ -0,0 +1,67 @@
+/*
+ * OpenCreative+, Minecraft plugin.
+ * (C) 2022-2025, McChicken Studio, mcchickenstudio@gmail.com
+ *
+ * OpenCreative+ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenCreative+ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ua.mcchickenstudio.opencreative.coding.blocks.conditions.entityconditions.other;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.NPC;
+import ua.mcchickenstudio.opencreative.coding.arguments.Arguments;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.Action;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.ActionType;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.Target;
+import ua.mcchickenstudio.opencreative.coding.blocks.conditions.entityconditions.EntityCondition;
+import ua.mcchickenstudio.opencreative.coding.blocks.executors.Executor;
+import ua.mcchickenstudio.opencreative.settings.Sounds;
+
+import java.util.List;
+
+public class EntityNameEqualsCondition extends EntityCondition {
+    public EntityNameEqualsCondition(Executor executor, Target target, int x, Arguments args, List<Action> actions, List<Action> reactions, boolean isOpposed) {
+        super(executor, target, x, args, actions, reactions, isOpposed);
+    }
+
+    @Override
+    public boolean check(Entity entity) {
+        boolean requiredColor = getArguments().getValue("color",false,this);
+        boolean requiredCaps = getArguments().getValue("caps",false,this);
+        List<String> names = getArguments().getTextList("names",this);
+        String entityName = entity.getName();
+        for (String name : names) {
+            if (!requiredColor) {
+                name = ChatColor.stripColor(name);
+                entityName = ChatColor.stripColor(entityName);
+            }
+            if (requiredCaps) {
+                if (entityName.equals(name)) {
+                    return true;
+                }
+            } else {
+                if (entityName.equalsIgnoreCase(name)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public ActionType getActionType() {
+        return ActionType.IF_ENTITY_NAME_EQUALS;
+    }
+}
