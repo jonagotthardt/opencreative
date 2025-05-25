f161966f6fd1c9f733b4518b979619f600b5cd81
nosatlev <mcchickenstudio@gmail.com>
2025-05-25 08:16:25 +0300

    Dev: Added Set Sheep Color, Set Dog's Collar Color actions

---
+++ src/main/java/ua/mcchickenstudio/opencreative/coding/blocks/actions/entityactions/state/EntitySetDogCollarColorAction.java
@@ -0,0 +1,53 @@
+/*
+ * OpenCreative+, Minecraft plugin.
+ * (C) 2022-2025, McChicken Studio, mcchickenstudio@gmail.com
+ *
+ * OpenCreative+ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenCreative+ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ua.mcchickenstudio.opencreative.coding.blocks.actions.entityactions.state;
+
+import org.bukkit.DyeColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Wolf;
+import ua.mcchickenstudio.opencreative.coding.arguments.Arguments;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.ActionType;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.Target;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.entityactions.EntityAction;
+import ua.mcchickenstudio.opencreative.coding.blocks.executors.Executor;
+
+public final class EntitySetDogCollarColorAction extends EntityAction {
+    public EntitySetDogCollarColorAction(Executor executor, Target target, int x, Arguments args) {
+        super(executor, target, x, args);
+    }
+
+    @Override
+    protected void execute(Entity entity) {
+        String colorName = getArguments().getValue("color", "red", this);
+        DyeColor color;
+        try {
+            color = DyeColor.valueOf(colorName.toUpperCase().replace("-","_"));
+        } catch (Exception ignored) {
+            color = DyeColor.RED;
+        }
+        if (entity instanceof Wolf wolf) {
+            wolf.setCollarColor(color);
+        }
+    }
+
+    @Override
+    public ActionType getActionType() {
+        return ActionType.ENTITY_SET_DOG_COLLAR_COLOR;
+    }
+}
---
+++ src/main/java/ua/mcchickenstudio/opencreative/coding/blocks/actions/entityactions/state/EntitySetSheepColorAction.java
@@ -0,0 +1,53 @@
+/*
+ * OpenCreative+, Minecraft plugin.
+ * (C) 2022-2025, McChicken Studio, mcchickenstudio@gmail.com
+ *
+ * OpenCreative+ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenCreative+ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ua.mcchickenstudio.opencreative.coding.blocks.actions.entityactions.state;
+
+import org.bukkit.DyeColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Sheep;
+import ua.mcchickenstudio.opencreative.coding.arguments.Arguments;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.ActionType;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.Target;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.entityactions.EntityAction;
+import ua.mcchickenstudio.opencreative.coding.blocks.executors.Executor;
+
+public final class EntitySetSheepColorAction extends EntityAction {
+    public EntitySetSheepColorAction(Executor executor, Target target, int x, Arguments args) {
+        super(executor, target, x, args);
+    }
+
+    @Override
+    protected void execute(Entity entity) {
+        String colorName = getArguments().getValue("color", "white", this);
+        DyeColor color;
+        try {
+            color = DyeColor.valueOf(colorName.toUpperCase().replace("-","_"));
+        } catch (Exception ignored) {
+            color = DyeColor.WHITE;
+        }
+        if (entity instanceof Sheep sheep) {
+            sheep.setColor(color);
+        }
+    }
+
+    @Override
+    public ActionType getActionType() {
+        return ActionType.ENTITY_SET_SHEEP_COLOR;
+    }
+}
