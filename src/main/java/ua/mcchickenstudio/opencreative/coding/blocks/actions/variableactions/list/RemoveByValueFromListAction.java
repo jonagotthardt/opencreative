a4bd1815949f64fb8bc4c55d7c9b2d40f17323c4
nosatlev <mcchickenstudio@gmail.com>
2025-09-25 20:28:45 +0300

    Added 1 variable action: Remove values from list

---
+++ src/main/java/ua/mcchickenstudio/opencreative/coding/blocks/actions/variableactions/list/RemoveByValueFromListAction.java
@@ -0,0 +1,95 @@
+/*
+ * OpenCreative+, Minecraft plugin.
+ * (C) 2022-2025, McChicken Studio, mcchickenstudio@gmail.com
+ *
+ * OpenCreative+ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenCreative+ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ua.mcchickenstudio.opencreative.coding.blocks.actions.variableactions.list;
+
+import org.bukkit.entity.Entity;
+import ua.mcchickenstudio.opencreative.coding.arguments.Arguments;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.ActionType;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.Target;
+import ua.mcchickenstudio.opencreative.coding.blocks.actions.variableactions.VariableAction;
+import ua.mcchickenstudio.opencreative.coding.blocks.executors.Executor;
+import ua.mcchickenstudio.opencreative.coding.exceptions.CollectionWithCollectionException;
+import ua.mcchickenstudio.opencreative.coding.variables.VariableLink;
+
+import java.util.*;
+
+public final class RemoveByValueFromListAction extends VariableAction {
+    public RemoveByValueFromListAction(Executor executor, Target target, int x, Arguments args) {
+        super(executor, target, x, args);
+    }
+
+    @Override
+    protected void execute(Entity entity) {
+
+        List<Object> list = new ArrayList<>(getArguments().getList("variable",this));
+        List<Object> elements = getArguments().getList("elements",this);
+        if (list.isEmpty() || elements.isEmpty()) return;
+        VariableLink variable = getArguments().getVariableLink("variable",this);
+        String deletionType = getArguments().getValue("deletion", "all", this);
+
+        if (cannotChangeListElements(elements.size())) {
+            return;
+        }
+
+        Set<Integer> indexes = new LinkedHashSet<>();
+        switch (deletionType.toLowerCase()) {
+            case "first" -> {
+                for (Object element : elements) {
+                    int index = list.indexOf(element);
+                    if (index != -1) indexes.add(index);
+                }
+            }
+            case "last" -> {
+                for (Object element : elements) {
+                    int index = list.lastIndexOf(element);
+                    if (index != -1) indexes.add(index);
+                }
+            }
+            default -> {
+                for (Object element : elements) {
+                    int index = 0;
+                    for (Object listElement : list) {
+                        if (element.equals(listElement)) {
+                            indexes.add(index);
+                        }
+                        index++;
+                    }
+                }
+            }
+        }
+
+        if (cannotChangeListElements(indexes.size())) {
+            return;
+        }
+        changeListElementsChangesAmount(indexes.size());
+
+        List<Integer> sortedIndexes = new ArrayList<>(indexes);
+        sortedIndexes.sort(Comparator.reverseOrder());
+        for (int index : sortedIndexes) {
+            if (index < list.size()) list.remove(index);
+        }
+        setVarValue(variable, list);
+
+    }
+
+    @Override
+    public ActionType getActionType() {
+        return ActionType.VAR_REMOVE_BY_VALUE_FROM_LIST;
+    }
+}
